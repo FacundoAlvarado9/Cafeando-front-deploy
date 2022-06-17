a7d8ad88dd99ee7386a8e779296d3025c557d217
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-06-17 16:30:10 -0300

    Robustez en consulta de tostadurias

--- src/pages/tostadurias.js
+++ src/pages/tostadurias.js
@@ -5,20 +5,25 @@ import TostaduriaCard from 'components/cards/TostaduriaCard'
 
 class Tostadurias extends React.Component {
     state = {
+      errored: false,    
       tostadurias: []
     }
 
     componentDidMount() {
       getAllTostadurias().then(res => {
         this.setState({ tostadurias: res.data.results })
+      }).catch((error) => {
+        this.setState({errored: true})
       })
     }
 
     render(){
         return(<>
+        
         <h1>Tostadurias 🏪</h1>
-        <div className='grid gap-3 justify-content-center'>
-          {this.state.tostadurias.map(tostaduria => <TostaduriaCard id={tostaduria.id} key={tostaduria.id} nombre={tostaduria.nombre}/>)}
+        <div className='grid gap-3 justify-content-center'> 
+          {this.state.errored ? (<p>uppss!</p>) : (       
+          this.state.tostadurias.map(tostaduria => <TostaduriaCard id={tostaduria.id} key={tostaduria.id} nombre={tostaduria.nombre}/>))}
         </div>
         </>)
     }
