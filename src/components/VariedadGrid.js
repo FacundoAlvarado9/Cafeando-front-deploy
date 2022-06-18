582ac8cef466dfdac9994a916eec6598ed0d2c36
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-06-18 03:14:57 -0300

    Cambios margenes

--- src/components/VariedadGrid.js
+++ src/components/VariedadGrid.js
@@ -7,15 +7,15 @@ class VariedadGrid extends React.Component {
 
     render(){        
         return(
-            <div className='flex flex-column'>
+            <div className='flex flex-column gap-5'>
                 <h1>{ this.props.titulo }</h1>
                 <p>{ this.props.descripcion }</p>
-                <div className="grid gap-3 justify-content-center">
+                <div className="flex grid gap-3 justify-content-center">
                     {this.props.variedades.map(variedad => {          
                     return <VariedadCard variedad={variedad} key={variedad["id"]}/>
                     })}            
                 </div>
-                <div>
+                <div className='flex justify-content-center'>
                     <Paginator first={this.props.startIndex} rows={this.props.pageSize} totalRecords={this.props.totalCount} rowsPerPageOptions={[2,3,5]} onPageChange={this.props.onPageChange}></Paginator>
                 </div>
             </div>
