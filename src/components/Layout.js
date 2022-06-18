b2a406559145d7af63dc7625f3679748b3ff7aa8
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-06-18 01:18:22 -0300

    Cambios estilo layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,24 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import Footer from "./Footer"
 
+//import Navbar from "./Navbar"
 import Navbar from "./Navbar"
+
+import './styles/layout.css'
 
 class Layout extends React.Component {
 
     render(){
-        return(<>            
-            <div className="content flex flex-column">   
-                <Navbar />
-                <main>
-                    <Outlet />  
-                </main>                  
-            </div>
+        return(<>
+            <div className="layout flex flex-column">
+                <div className="">
+                    <Navbar/>
+                </div>
+                <div className="layout-content">
+                    <Outlet />                  
+                </div>
+            </div>            
         </>)
     }    
 
