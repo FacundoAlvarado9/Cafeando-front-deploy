86e5f388d2f00906661d19240cb76a4701f283b2
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-06-18 01:26:04 -0300

    Robustez para las variedades de una tostaduria

--- src/pages/tostadurias/singleTostaduria.js
+++ src/pages/tostadurias/singleTostaduria.js
@@ -10,6 +10,7 @@ import OrigenesDropdown from 'components/OrigenesDropdown'
 import { Button } from 'primereact/button'
 import SucursalCard from 'components/cards/SucursalCard'
 import VariedadGrid from 'components/VariedadGrid'
+import Error from 'components/Error'
 
 export default function SingleTostaduria() {
   
@@ -25,24 +26,31 @@ export default function SingleTostaduria() {
     searchString: "",
     origen: null
   })
+  const [errored, setErrored] = useState(false)
 
   const { tost_id }= useParams()
 
   useEffect(() => {
     getSingleTostaduria(tost_id).then(res => {
       setTostaduria(res.data["results"][0])
-    }) 
+    }).catch((error) => {
+      setErrored(true)
+    })
 
     getSucursalesFromSingleTostaduria(tost_id).then(res => {
       setSucursales(res.data["results"])
-    }) 
+    }).catch((error) => {
+      setErrored(true)
+    })
   }, [tost_id])
 
   useEffect(() => {         
     getVariedadesFromTostaduria(tost_id, filters).then(resVariedades => {
       setVariedades(resVariedades.data["results"])
       setTotalCount(resVariedades.data["totalCount"])
-    })    
+    }).catch((error) => {
+      setErrored(true)
+    })
   }, [filters, tost_id])
 
   const handlePageClick = (event) => {
@@ -66,48 +74,49 @@ export default function SingleTostaduria() {
       origen: newOrigen
     }))
   }
-  console.log("total count: " + totalCount)
 
-  const backup = <>
-    <div className='flex flex-column'>
-            <h1>{ tostaduria.nombre }</h1>
-            <div className="grid gap-3 justify-content-center">
-                {variedades.map(variedad => {          
-                  return <VariedadCard variedad={variedad} key={variedad["id"]}/>
-                })}
-            </div> 
+  const mostrarVariedadesTostaduria = <>
+      <div className='flex flex-column'>
+        <div className='flex flex-column md:flex-row gap-0 md:gap-3 justify-content-center align-items-center'>
+          <div className="flex">
+            <Link to="/tostadurias" className="link"><Button icon="pi pi-chevron-left" label="Volver"/></Link>
+          </div>
+          <div className="flex">
+            <Search value={filters["searchString"]} onChange={changeSearchString} name="texto" placeholder="Buscar"/>
+          </div>
+          <div className="flex">
+            <OrigenesDropdown onChange={changeOrigenFilter} />
+          </div>                    
+        </div>
+
+          <VariedadGrid variedades={variedades}
+                titulo={ tostaduria["nombre"] }
+                startIndex={ filters["startIndex"] }
+                pageSize={ filters["pageSize"] }
+                totalCount={totalCount}
+                onPageChange={handlePageClick}
+          />
+        <div className='flex flex-column'>
+          <h2>Sucursales</h2>
+          <div className="grid gap-3 justify-content-center">
+              {sucursales.map(sucursal => {          
+                return <SucursalCard sucursal={sucursal} key={sucursal["id"]}/>
+              })}
           </div> 
+        </div>       
+      </div>
   </>
 
-  return(
-    <div className='flex flex-column'>
-      <div className='flex flex-column md:flex-row gap-0 md:gap-3 justify-content-center align-items-center'>
-        <div className="flex">
-          <Link to="/tostadurias" className="link"><Button icon="pi pi-chevron-left" label="Volver"/></Link>
-        </div>
-        <div className="flex">
-          <Search value={filters["searchString"]} onChange={changeSearchString} name="texto" placeholder="Buscar"/>
-        </div>
-        <div className="flex">
-          <OrigenesDropdown onChange={changeOrigenFilter} />
-        </div>                    
-      </div>
+  let result
 
-         <VariedadGrid variedades={variedades}
-              titulo={ tostaduria["nombre"] }
-              startIndex={ filters["startIndex"] }
-              pageSize={ filters["pageSize"] }
-              totalCount={totalCount}
-              onPageChange={handlePageClick}
-         />
-      <div className='flex flex-column'>
-        <h2>Sucursales</h2>
-        <div className="grid gap-3 justify-content-center">
-            {sucursales.map(sucursal => {          
-              return <SucursalCard sucursal={sucursal} key={sucursal["id"]}/>
-            })}
-        </div> 
-      </div>       
-    </div>
+  if(!errored){
+    result = mostrarVariedadesTostaduria
+  } else {
+    result = <Error volver="/tostadurias" />
+  }
+
+
+  return(
+    result
   )
 }
